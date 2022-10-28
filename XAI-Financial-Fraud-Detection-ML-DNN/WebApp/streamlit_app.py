24309b6b738bbe2dc7f9c495e4d6884885dc4d63
coder-freak <82790440+coder-freak@users.noreply.github.com>
2022-10-28 12:35:48 +0530

    Update streamlit_app.py

--- XAI-Financial-Fraud-Detection-ML-DNN/WebApp/streamlit_app.py
+++ XAI-Financial-Fraud-Detection-ML-DNN/WebApp/streamlit_app.py
@@ -33,7 +33,7 @@ def main():
   st.markdown(html_temp, unsafe_allow_html=True)
  
   from PIL import Image
-  image = Image.open('/content/home_banner.PNG')
+  image = Image.open('XAI-Financial-Fraud-Detection-ML-DNN/WebApp/home_banner.PNG')
 
   st.image(image, caption='Impacting the World of Finance and Banking with Artificial Intelligence (AI)')
 
